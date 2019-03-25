bba1e886479e328256fd3720c8beab4c9f75c507
Sam Ireland <sam.ireland.uk@gmail.com>
2019-03-25 11:28:02 +0000

    Simplify

--- atomium.cpp
+++ atomium.cpp
@@ -1,34 +1,7 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
 using namespace std;
 
 int main(int argc, char *argv[]) {
-
-  // What is the file to open?
-  string fileName;
-  if (argc < 2) {
-    cout << "Where is the pdb?" << endl;
-    cin >> fileName;
-  } else {
-    fileName = argv[1];
-  }
-
-  // Get file contents
-  vector<string> pdbLines(0);
-  string pdbLine;
-  ifstream infile;
-  infile.open(fileName);
-  if (infile.good()) {
-    while(!infile.eof()) {
-      getline(infile, pdbLine);
-      pdbLines.push_back(pdbLine);
-    }
-  } else {
-    cout << "Could not find that file" << endl;
-  }
-  infile.close();
-
+  cout << "Welcome to atomium++" << endl;
   return 0;
 }
