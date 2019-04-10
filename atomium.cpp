ddaf987914c39aef6f0dce585a213db9d3a76f46
Sam Ireland <sam.ireland.uk@gmail.com>
2019-04-10 14:54:44 +0100

    Add string

--- atomium.cpp
+++ atomium.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "pdb.h"
 using namespace std;
 
 int main(int argc, char *argv[]) {
   cout << "Welcome to atomium++" << endl;
+  cout << "The test PDB is " << pdb.length() << " bytes long" << endl;
   return 0;
 }
