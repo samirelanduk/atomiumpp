0d2bd65590dcdde52560d8f26945a6c3de504673
Sam Ireland <sam.ireland.uk@gmail.com>
2019-05-26 09:47:54 +0100

    Atom constructor

--- atomium.cpp
+++ atomium.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
   cout << "Welcome to atomium++" << endl;
 
   // Make atom
-  Atom* atom = new Atom();
+  Atom* atom = new Atom("N", 0, 0, 0, 1, "N", 0.5, 0.6);
+
   return 0;
 }
--- structures.h
+++ structures.h
@@ -9,4 +9,19 @@ class Atom {
   float charge;
   float bvalue;
   float anisotropy[6];
+
+  public: Atom(string element_, float x_, float y_, float z_, int id_, string name_, float charge_, float bvalue_) {
+    element = element_;
+    location[0] = x_;
+    location[1] = y_;
+    location[2] = z_;
+    id = id_;
+    name = name_;
+    charge = charge_;
+    bvalue = bvalue_;
+  }
+
+  public: string getElement() {
+    return element;
+  }
 };
